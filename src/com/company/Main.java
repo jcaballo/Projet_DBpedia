e4646ad4c6e88450ac001c8a903de6b2d83016c1
caballoj <julien.caballol@hotmail.fr>
2014-12-03 09:28:40 +0100

    Ajout de la librairie et du début du main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import org.openrdf.query.*;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+
+        HTTPRepository repo = new HTTPRepository("http://dbpedia.org/sparql", "");
+        RepositoryConnection connection = repo.getConnection();
+
+        TupleQuery query= connection.prepareTupleQuery(QueryLanguage.SPARQL, "select distinct ?Concept wher {?x a ?Concept} LIMIT 10");
+        TupleQueryResult result=query.evaluate();
+        while (result.hasNext()) {
+            BindingSet bindset = result.next();
+            System.out.println(bindset);
+            System.out.println("Concept :");
+            System.out.println(bindset.getValue("Concept"));
+        }
+
     }
+
 }
+
