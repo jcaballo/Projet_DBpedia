94732ae368a42bf3bde7ee5d89bf0c8bc998708c
caballoj <julien.caballol@hotmail.fr>
2014-12-09 10:27:51 +0100

    Panel qui s'ouvre

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,31 +6,44 @@ import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.http.HTTPRepository;
 
+import javax.swing.*;
+
 public class Main {
 
-    /*public static void main(String[] args) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+    public static void main(String[] args) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
 
-        HTTPRepository repo = new HTTPRepository("http://dbpedia.org/sparql", "");
+        HTTPRepository repo = new HTTPRepository("http://fr.dbpedia.org/sparql", "");
         RepositoryConnection connection = repo.getConnection();
 
-        TupleQuery query= connection.prepareTupleQuery(QueryLanguage.SPARQL, "select distinct ?Concept where {?x a ?Concept} LIMIT 10");
+        TupleQuery query= connection.prepareTupleQuery(QueryLanguage.SPARQL, "select distinct * where {\n" +
+                "?s rdfs:label \"Victor Hugo\"@fr.\n" +
+                "?s prop-fr:décès ?deces .\n" +
+                "?s prop-fr:dateDeNaissance ?naissance\n" +
+                "} LIMIT 100");
         TupleQueryResult result=query.evaluate();
         while (result.hasNext()) {
             BindingSet bindset = result.next();
-            System.out.println(bindset);
-            System.out.println("Concept :");
-            System.out.println(bindset.getValue("Concept"));
+            //System.out.println(bindset);
+            System.out.println("ICI :");
+            System.out.println("From : " + bindset.getValue("naissance") + " to " + bindset.getValue("deces"));
         }
 
-        query = connection.prepareTupleQuery(QueryLanguage.SPARQL, "Select ?x ?foafName WHERE {?x <http://xmlns.com/foaf/0.1/name> ?foafName} LIMIT 10");
+        /*query = connection.prepareTupleQuery(QueryLanguage.SPARQL, "Select ?x ?foafName WHERE {?x <http://xmlns.com/foaf/0.1/name> ?foafName} LIMIT 10");
         result=query.evaluate();
         while(result.hasNext()) {
             BindingSet bindset = result.next();
             System.out.println(bindset.getValue("foafName"));
-        }
+        }*/
         connection.close();
 
-    }*/
+        JFrame frame = new JFrame("Dbpedia");
+        frame.setContentPane(new Dbpedia().getPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
 
 }
 
